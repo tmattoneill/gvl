5946f6e4415d24e2a9704bd0053bf0c8ddc96669
Matt O'Neill <tmattoneill@gmail.com>
2020-07-29 11:34:34 +0100

    initial load up of files

--- tools/csvtojson.py
+++ tools/csvtojson.py
@@ -3,31 +3,42 @@ from   datetime import datetime as dt
 import time
 import os
 
-file            = "../data/gid_domain_list.csv"
-json_fh         = "../data/gid_domains.json"
-json_data       = {}
-csv_create_time = os.path.getmtime(file)
-td_format       = "%Y-%m-%d %H:%M:%SZ"
+# Initialise the script and set defaults
+csv_file_location    = "../data/gid_domain_list.csv" # location of CSV
+json_output_location = "../data/gid_domains.json"    # location for output
+csv_create_time      = os.path.getmtime(csv_file_location)
+td_format            = "%Y-%m-%d %H:%M:%SZ"
+cnt_gid              = 0
+cnt_dom              = 0
+delim                = "\t"
 
-json_data["dataSource"]     = file
+# Set up the meta data and overall JSON file structure
+json_data                   = {}
+json_data["dataSource"]     = csv_file_location
 json_data["jsonUpdateTime"] = dt.strftime(dt.now(),td_format)
-json_data["csvCreateTime"]  = time.strftime(td_format, time.localtime(csv_create_time))
+json_data["csvCreateTime"]  = time.strftime(td_format, 
+                                            time.localtime(csv_create_time))
 json_data["data"]           = {}
 
-with open(file, encoding='utf-8-sig', newline='\n') as csv_file:
+with open(csv_file_location, encoding='utf-8-sig', newline='\n') as csv_file:
     csvReader = csv.DictReader(csv_file)
 
     for rows in csvReader:
         
         if rows['gid'].strip() is not None and rows['gid'] != '':
+            cnt_gid += 1
+            cnt_dom += 1
             gid = rows['gid']
             json_data['data'][gid] = { 
                         "company_name": rows['company_name'],
                         "domains": [rows['domain']]
             }
         else:
+            cnt_dom += 1
             json_data['data'][gid]["domains"].append(rows['domain'])
 
 
-with open(json_fh, 'w') as json_file:
+with open(json_output_location, 'w') as json_file:
     json_file.write(json.dumps(json_data, indent=4))
+
+print (f"Found {cnt_gid} unique GIDs and {cnt_dom} domains.\n")
